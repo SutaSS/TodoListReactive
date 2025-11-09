ff55dcf9da9e75d4e90da6a125a1425a8fe863bc
Kirukato936 <ivandiamond2004@gmail.com>
2025-11-09 21:19:32 +0700

    Update Todo.kt

--- app/src/main/java/com/example/todolistreactive/model/Todo.kt
+++ app/src/main/java/com/example/todolistreactive/model/Todo.kt
@@ -1,2 +1,7 @@
 package com.example.todolistreactive.model
 
+data class Todo (
+val id: Int, // ID unik untuk setiap tugas [cite: 42]
+val title: String, // Judul tugas [cite: 43]
+val isDone: Boolean = false // Status selesai, default-nya false (belum selesai) [cite: 44]
+)
